6ffe1a65d1b4f91053e97c33f6d27d954a2ce0d5
birkhoff <git@birkhoff.me>
2026-04-20 00:02:37 +0200

    update claude
    Signed-off-by: birkhoff <git@birkhoff.me>

--- home/programs/claude.nix
+++ home/programs/claude.nix
@@ -1,9 +1,14 @@
 { ... }:
 {
   # GitHub MCP Setup:
-  # $ claude mcp add-json github '{"type":"http","url":"https://api.githubcopilot.com/mcp","headers":{"Authorization":"Bearer YOUR_GITHUB_PAT"}}'
+  # claude mcp add-json github '{"type":"http","url":"https://api.githubcopilot.com/mcp","headers":{"Authorization":"Bearer YOUR_GITHUB_PAT"}}'
 
   # Exa
+  # claude mcp add --transport http exa https://mcp.exa.ai/mcp
 
   # Ref
+  # @see https://ref.tools/mcp
+
+  # alphaXiv
+  # claude mcp add --transport http alphaxiv https://api.alphaxiv.org/mcp/v1
 }
